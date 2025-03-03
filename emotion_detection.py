4ba40c309c1028b3a78baaba3913e2816aeea01b
lucasdresie <119262524+lucasdresie@users.noreply.github.com>
2025-03-03 17:16:07 -0500

    Added comments

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,6 @@
 import requests
 
+#Emotion detection function
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
