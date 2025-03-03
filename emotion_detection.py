7368adaac311a65ef0e7744b6a1b23fbf85bb963
lucasdresie <119262524+lucasdresie@users.noreply.github.com>
2025-03-03 17:28:30 -0500

    Added task 3 code

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 #Emotion detection function
 def emotion_detector(text_to_analyze):
@@ -6,4 +7,29 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
-    return response.text
+    format_response = json.loads(response.text)
+
+
+    #Extract emotions and their scores:
+    anger_score = formatted_response["emotionPredictions"][0]["emotion"]["anger"]
+    disgust_score = formatted_response["emotionPredictions"][0]["emotion"]["disgust"]
+    fear_score = formatted_response["emotionPredictions"][0]["emotion"]["fear"]
+    joy_score = formatted_response["emotionPredictions"][0]["emotion"]["joy"]
+    sadness_score = formatted_response["emotionPredictions"][0]["emotion"]["sadness"]
+
+    #Determine the dominant emotion with the highest score:
+    emotion_list = [anger_score, disgust_score, fear_score, joy_score, sadness_score]
+    dominant_emotion_index = emotion_list.index(max(emotion_list))
+    emotion_keys = ["anger", "disgust", "fear", "joy", "sadness"]
+    dominant_emotion_key = emotion_keys[dominant_emotion_index]
+    #Format the response into the required output format: 
+    result = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': '<name of the dominant emotion>'
+    }
+
+    return result
